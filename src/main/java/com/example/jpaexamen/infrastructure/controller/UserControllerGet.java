05b6a7ee4a9647f517e68ea7e9da1d68abf58598
SergioLF1 <sergio.lopez@bosonit.com>
2021-04-30 13:43:21 +0200

    Funciona CRUD

---
+++ src/main/java/com/example/jpaexamen/infrastructure/controller/UserControllerGet.java
@@ -0,0 +1,73 @@
+package com.example.jpaexamen.infrastructure.controller;
+
+import com.example.jpaexamen.domain.DTOUser;
+import com.example.jpaexamen.domain.User;
+import com.example.jpaexamen.infrastructure.repository.UserPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping({"/user"})
+public class UserControllerGet {
+    @Autowired
+    UserPort userPort;
+
+    //Get All
+    @GetMapping("/get")
+    public List<DTOUser> getAllContacts() {
+        List<User> list = userPort.findAll();
+        List<DTOUser> list1 = new ArrayList<DTOUser>();
+        DTOUser dtoUser;
+        for (User user : list) {
+            dtoUser = new DTOUser(user);
+            list1.add(dtoUser);
+        }
+        return list1;
+    }
+
+    //Add
+    @PostMapping("/add")
+    public User agregar(@RequestBody DTOUser dtoUser) {
+        User user = new User(dtoUser);
+        return userPort.save(user);
+    }
+
+    //Get by Id
+    @GetMapping("/get/{id}")
+    public DTOUser getOneDto(@PathVariable String id) throws Exception {
+        User user = userPort.findById(id).orElseThrow(() -> new Exception("Not found"));
+        DTOUser dtoUser = new DTOUser(user);
+        return dtoUser;
+    }
+
+    //Delete
+    @DeleteMapping(value = "/delete/{id}")
+    public void borrarUsuarioById(@PathVariable String id) {
+        Optional<User> c = userPort.findById(id);
+        if (c.isPresent()) {
+            userPort.deleteById(id);
+        }
+    }
+
+    //Edit
+    @PutMapping(value = "/edit/{id}")
+    public ResponseEntity<DTOUser> updateUser(@PathVariable("id") String id, @RequestBody DTOUser dtoUser) {
+        Optional<User> userData = userPort.findById(id);
+        if (userData.isPresent()) {
+            User user = userData.get();
+            user.convert(dtoUser);
+            userPort.save(user);
+            return new ResponseEntity<>(dtoUser, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+}
