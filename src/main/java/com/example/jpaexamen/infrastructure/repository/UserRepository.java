2e1231d48dafa382e6c147636eb33672104e0308
SergioLF1 <sergio.lopez@bosonit.com>
2021-05-11 13:04:12 +0200

    cambio de DTO

--- src/main/java/com/example/jpaexamen/infrastructure/repository/UserRepository.java
+++ src/main/java/com/example/jpaexamen/infrastructure/repository/UserRepository.java
@@ -3,9 +3,6 @@ package com.example.jpaexamen.infrastructure.repository;
 import com.example.jpaexamen.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.HashMap;
-import java.util.List;
-
-public interface UserRepository extends JpaRepository<User,String> {
-     //List<User> getData(HashMap<String, Object> conditions);
+public interface UserRepository extends JpaRepository<User, String> {
+    //List<User> getData(HashMap<String, Object> conditions);
 }
